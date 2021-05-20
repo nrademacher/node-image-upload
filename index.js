0fa0973d846d9796cd2874c8a00417b1d01750db
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-20 18:15:01 +0200

    feat: implement cloudinary and mongodb storage

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const multer = require('multer');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads/images');
+    },
+    filename: function (req, file, callback) {
+        callback(null, Date.now() + '.jpg'); //Appending .jpg
+    },
+});
+
+const upload = multer({ storage: storage }).single('photo');
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.post('/upload', upload, (req, res) => {
+    if (!req.file) {
+        console.log('No file received');
+        return res.send({
+            success: false,
+        });
+    } else {
+        console.log('file received');
+        return res.send({
+            success: true,
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Listening at ' + PORT);
+});
+
