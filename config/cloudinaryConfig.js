6ad4913b97affc22dfeafc4f2458f0027fb569e0
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-20 19:56:58 +0200

    chore: cleanup

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const cloudinary = require('cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME || YOUR_CLOUD_NAME,
-    api_key: process.env.API_KEY || YOUR_API_KEY,
-    api_secret: process.env.API_SECRET || YOUR_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
 exports.uploads = (file) => {
