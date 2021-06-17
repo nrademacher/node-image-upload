7139611beddca87bed49fddab83a836ea571ea12
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-18 00:30:00 +0200

    feat: update image model with album

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose'),
-    { Schema } = mongoose,
-    imageSchema = new Schema({
-        file: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-      album: {
-        type: string,
-        required: true,
-      },
-        location: {
-            type: String,
-        },
-        imageId: {
-            type: String,
-        },
-    });
+  { Schema } = mongoose,
+  imageSchema = new Schema({
+    file: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    album: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    imageId: {
+      type: String,
+    },
+  });
 
 module.exports = mongoose.model('Image', imageSchema);
