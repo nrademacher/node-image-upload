614d15f729e7177a05ed6fc361fe1ea0fc235fb3
Nikolay Rademacher <hrrdx33@gmail.com>
2021-07-04 20:10:41 +0200

    test: set cors policy

--- server.js
+++ server.js
@@ -9,22 +9,23 @@ const router = require('./routes/index');
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 let db = mongoose.connection;
 db.on('open', () => {
-    console.log('Connected to MongoDB using Mongoose');
+  console.log('Connected to MongoDB using Mongoose');
 });
 
 process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }))
-    .use(express.json())
-    .use(cors())
-    //Index route
-    .use('/', router)
-    .set('port', process.env.PORT || 8000)
-    .listen(app.get('port'), () => {
-        console.log(`Server running at http://localhost:${app.get('port')}`);
-    });
+app
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json())
+  .use(cors({ origin: '*' }))
+  //Index route
+  .use('/', router)
+  .set('port', process.env.PORT || 8000)
+  .listen(app.get('port'), () => {
+    console.log(`Server running at http://localhost:${app.get('port')}`);
+  });
