c1b35a2f764643cc35ecef6264819053d3df33a9
Nikolay Rademacher <hrrdx33@gmail.com>
2021-07-10 00:48:00 +0200

    chore: remove console log

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -18,7 +18,7 @@ module.exports = {
       };
       console.log(item);
       const match = await imageModel.findOne({
-        title: item.oldTitle,
+        title: req.body.oldTitle,
         album: item.album,
       });
       console.log(match);
